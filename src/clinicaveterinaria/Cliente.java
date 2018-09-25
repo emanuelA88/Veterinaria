ff577eec5ba80d56996168dff459adde352a8033
Lucero Mathias <mathiaz1338@gmail.com>
2018-09-25 17:31:10 -0300

    Segundo Commit

--- src/clinicaveterinaria/Cliente.java
+++ src/clinicaveterinaria/Cliente.java
@@ -1,14 +1,93 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package clinicaveterinaria;
 
-/**
- *
- * @author ML
- */
 public class Cliente {
+
+    
+    private int id = -1;
+    private String dni;
+    private String apellido;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String personaAlternativa;
+    
+    public Cliente(int id, String dni, String apellido, String nombre, String direccion, String telefono, String personaAlternativa) {
+        this.id = id;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.personaAlternativa = personaAlternativa;
+    }
+  
+    public Cliente(String dni, String apellido, String nombre, String telefono) {
+        this.id = -1;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+    
+    public Cliente (){
+        this.id = -1;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPersonaAlternativa() {
+        return personaAlternativa;
+    }
+
+    public void setPersonaAlternativa(String personaAlternativa) {
+        this.personaAlternativa = personaAlternativa;
+    }
     
 }
--- src/clinicaveterinaria/Conexion.java
+++ src/clinicaveterinaria/Conexion.java
@@ -1,60 +1,35 @@
 package clinicaveterinaria;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 
 
 public class Conexion {
+    private String url;
+    private String usuario;
+    private String password;
+
+    private Connection conexion;
     
-    public static void main(String[] args){
-        
-        String url = "jdbc:mysql://localhost/veterinaria";
-        String usuario = "root";
-        String password = "";
-        Connection conexion;
-        PreparedStatement ps;
-        
-        try {
-            Class.forName("org.mariadb.jdbc.Driver");
-            conexion = DriverManager.getConnection(url,usuario,password);
-            
-        //CREATE
-            ps = conexion.prepareStatement("INSERT INTO cliente (nombre) VALUES ( ? );");
-            ps.setString(1, "Lucero");
-            ps.executeUpdate();
-            ps.close();
-        
-        //UPDATE
-                /*ps=conexion.prepareStatement("UPDATE alumno SET activo=0");
-                ps.executeUpdate();
-                ps.close();*/
-        
-        //DELETE
-        /*ps=conexion.prepareStatement("DELETE FROM alumno where ID=?;");
-        ps.setInt(1, 2);
-        ps.executeUpdate();
-        ps.close();*/
-        
-        //READ
-        /*ps=conexion.prepareStatement("SELECT * FROM alumno");
-        ResultSet rs= ps.executeQuery();
-        while(rs.next()){
-            System.out.println("Nombre: "+rs.getString(2));
-        }
-        ps.close();*/
-        
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    
+    public Conexion(String url, String usuario, String password) throws ClassNotFoundException {
+        this.url = url;
+        this.usuario = usuario;
+        this.password = password;
+
+        //Cargamos las clases de mariadb que implementan JDBC
+        Class.forName("org.mariadb.jdbc.Driver");
+
     }
+    
+    public Connection getConexion() throws SQLException{
+        if(conexion == null){
+                    // Setup the connection with the DB
+            conexion = DriverManager
+                .getConnection(url + "?useLegacyDatetimeCode=false&serverTimezone=UTC"
+                        + "&user=" + usuario + "&password=" + password);
+        }
+        return conexion;
+    }
+    
 }
